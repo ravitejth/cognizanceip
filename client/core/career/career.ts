ba51a0bb6e3d21c256eac4a8fe9e45670c3fb567
DESKTOP-TKTOPNN\Ravi <ravitej.th@gmail.com>
2017-11-20 00:40:00 +0530

    career page.

--- client/core/career/career.ts
+++ client/core/career/career.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class CareerComponent {
+  private expanded1=false;
+  private expanded2=false;
+  private expanded3=false;
+
+  isexpanded1(){
+    this.expanded1 = !this.expanded1;
+  }
+  isexpanded2(){
+    this.expanded2 = !this.expanded2;
+  }
+  isexpanded3(){
+    this.expanded3 = !this.expanded3;
+  }
 }
