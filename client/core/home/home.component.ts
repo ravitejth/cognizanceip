6a9a6fa5255ea9ba09fb8c428d9f03295b93b899
DESKTOP-TKTOPNN\Ravi <ravitej.th@gmail.com>
2017-11-22 00:54:53 +0530

    client logos

--- client/core/home/home.component.ts
+++ client/core/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
     selector: 'home',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent  {
     name = 'home';
+
+	ngAfterViewInit() {
+
+        $('.carousel[data-type="multi"] .item').each(function() {
+            var next = $(this).next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+        
+            for (var i = 0; i < 2; i++) {
+                next = next.next();
+                if (!next.length) {
+                    next = $(this).siblings(':first');
+                }
+        
+                next.children(':first-child').clone().appendTo($(this));
+            }
+        });
+    }
 }
