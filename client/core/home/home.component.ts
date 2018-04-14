eafd7fa691e251f13d30e3c2769aac9c7c8e045f
Ravitej <raviteja@life9sys.com>
2018-04-14 17:50:16 +0530

    grey icons

--- client/core/home/home.component.ts
+++ client/core/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent  {
             }
             next.children(':first-child').clone().appendTo($(this));
         
-            for (var i = 0; i < 2; i++) {
+            for (var i = 0; i < 3; i++) {
                 next = next.next();
                 if (!next.length) {
                     next = $(this).siblings(':first');
