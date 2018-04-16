d04ae712c31516f174a1eee2e53c79c94bf77840
Ravitej <raviteja@life9sys.com>
2018-04-16 23:31:00 +0530

    time zone field

--- client/core/contact/contact.ts
+++ client/core/contact/contact.ts
@@ -11,14 +11,18 @@ export class ContactComponent {
   lat = 30.726107;
   lng = 76.773505;
   z_level = 14;
-  dateToday = new Date();
-
+  zones = ['UTC -07:00 Pacific Time','UTC -06:00 Mountain Time','UTC -05:00 Central Time','UTC -04:00 Eastern Time',
+          'UTC +01:00 Dublin, London, Lisbon','UTC +02:00 Central European Time','UTC +02:00 Kaliningrad','UTC +03:00 Eastern European Time',
+          'UTC +03:00 Chisinau and Tiraspol','UTC +03:00 Istanbul, Kirov, Minsk, and Moscow','UTC +04:00 Astrakhan, Samara, Saratov, and Ulyanovsk',
+          'UTC -07:00 Pacific and Yukon','UTC -06:00 Mountain','UTC -06:00 Saskatchewan','UTC -05:00 Central','UTC -04:00 Eastern',
+          'UTC -03:00 Atlantic Time','UTC -02:30 Newfoundland','UTC +00:00 Greenwich Mean Time','UTC +01:00 Central European Time',
+          'UTC +01:00 Casablanca and El Aaiun']
+  
   ngOnInit(){
     $('.datepicker').pickadate({
       selectMonths: true, // Creates a dropdown to control month
       selectYears: 15, // Creates a dropdown of 15 years to control year,
       today: 'Today',
-      minDate: this.dateToday,
       clear: 'Clear',
       close: 'Ok',
       closeOnSelect: false // Close upon selecting a date,
